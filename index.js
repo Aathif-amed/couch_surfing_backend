e90989664baeaf9eb9810475604cd6b14f5296c4
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-03 18:34:58 +0530

    routes and db connection imported

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import roomRoute from "./routes/roomRoute.js";
+import connection from "./utils/db.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -8,17 +11,37 @@ const port = process.env.PORT;
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.setHeader(
-    "Access-Control-Allow-HEADERS",
-    "X-Requested-With, Content-Type,Authorization"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Content-Type, Authorization'
   );
   next();
 });
+app.use(express.json({ limit: "10mb" }));
 
-app.use(express.json({limit:'10mb'}))
+app.use("/api/room", roomRoute);
+app.use("/api/user", userRoute);
 
-app.use('/',(req,res)=>{
-    return res.send("Welcome To Couch Surfing API")
-})
+app.get("/", (req, res) => {
+  return res.send("Welcome To Couch Surfing API");
+});
+app.use((req, res) => {
+  return res.status(404).json({ success: false, message: "Not Found" });
+});
+
+const startServer= async()=>{
+  try {
+    connection();
+    app.listen(port, () => {
+      console.log(`Server is listening on port:${port}`);
+    });
+  } catch (error) {
+    
+  }
+}
+startServer();
