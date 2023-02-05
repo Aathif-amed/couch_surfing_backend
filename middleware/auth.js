a9de000c06e9c05381fb2566f13ab8c242786015
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-05 17:22:57 +0530

    jwt token verification added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 import { OAuth2Client } from "google-auth-library";
+import jwt from 'jsonwebtoken'
 
 const client= new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
@@ -17,8 +18,11 @@ const auth= async(req,res,next)=>{
             req.user={id:payload.sub,name:payload.name,photoURL:payload.picture}
         }
         else{
-            // custom jwt token -for registered users
-        }
+           const decodedToken= jwt.verify(token,process.env.JWT_SECRET)
+           const { id,fName, lName, photoURL }=decodedToken
+           req.user={id,fName,lName,photoURL}
+        
+            }
         next()
     } catch (error) {
         console.log(error);
