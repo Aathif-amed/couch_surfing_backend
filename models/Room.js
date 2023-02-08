df3a4ac9218a05172803361ca00b59eb09822ce1
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-08 20:57:17 +0530

    room schema created

---
+++ models/Room.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+    longitude:{
+        type:Number,
+        required:true
+    },
+    latitude:{
+        type:Number,
+        required:true
+    },
+    price:{
+        type:Number,
+        min:50,
+        max:500,
+        default:0
+    },
+    title:{
+        type:String,
+        required:true,
+        minLength:5,
+        maxLength:150
+    },
+    description:{
+        type:String,
+        required:true,
+        minLength:10,
+        maxLength:1000
+    },
+    images:{
+        type:[String],
+        validate:(value)=>Array.isArray(value)&& value.length>0
+    },
+    uid:{
+        type:String,
+        required:true
+    },
+    uFname:{
+        type:String,
+        required:true
+    },
+    uLname:{
+        type:String,
+        default:''
+    },
+    uPhoto:{
+        type:String,
+        default:''
+    },
+},{timestamps:true})
+
+const Room= mongoose.model("rooms",roomSchema);
+
+export default Room
