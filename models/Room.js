ec3f33f087cd89910f8164c7be87b6f171f60782
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-10 21:46:44 +0530

    correction- min value changed

--- models/Room.js
+++ models/Room.js
@@ -1,54 +1,57 @@
 import mongoose from "mongoose";
 
-const roomSchema = new mongoose.Schema({
-    longitude:{
-        type:Number,
-        required:true
+const roomSchema = new mongoose.Schema(
+  {
+    longitude: {
+      type: Number,
+      required: true,
     },
-    latitude:{
-        type:Number,
-        required:true
+    latitude: {
+      type: Number,
+      required: true,
     },
-    price:{
-        type:Number,
-        min:50,
-        max:500,
-        default:0
+    price: {
+      type: Number,
+      min: 0,
+      max: 500,
+      default: 0,
     },
-    title:{
-        type:String,
-        required:true,
-        minLength:5,
-        maxLength:150
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 150,
     },
-    description:{
-        type:String,
-        required:true,
-        minLength:10,
-        maxLength:1000
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 1000,
     },
-    images:{
-        type:[String],
-        validate:(value)=>Array.isArray(value)&& value.length>0
+    images: {
+      type: [String],
+      validate: (value) => Array.isArray(value) && value.length > 0,
     },
-    uid:{
-        type:String,
-        required:true
+    uid: {
+      type: String,
+      required: true,
     },
-    uFname:{
-        type:String,
-        required:true
+    uFname: {
+      type: String,
+      required: true,
     },
-    uLname:{
-        type:String,
-        default:''
+    uLname: {
+      type: String,
+      default: "",
     },
-    uPhoto:{
-        type:String,
-        default:''
+    uPhoto: {
+      type: String,
+      default: "",
     },
-},{timestamps:true})
+  },
+  { timestamps: true }
+);
 
-const Room= mongoose.model("rooms",roomSchema);
+const Room = mongoose.model("rooms", roomSchema);
 
-export default Room
+export default Room;
