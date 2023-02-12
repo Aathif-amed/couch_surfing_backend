0c031790baa51d9324495ce300b19400103892f9
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-12 23:07:21 +0530

    function to update rooms added

--- controllers/room.js
+++ controllers/room.js
@@ -11,6 +11,19 @@ export const getRooms = tryCatch(async (req, res) => {
   const rooms = await Room.find().sort({ _id: -1 });
   res.status(200).json({ success: true, result: rooms });
 });
+export const updateRoom = tryCatch(async (req, res) => {
+  const updatedRoom = await User.findByIdAndUpdate(
+    req.params.roomId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  return res.status(200).json({
+    success: true,
+    result: updatedRoom,
+  });
+});
 export const deleteRoom = tryCatch(async (req, res) => {
   const { _id } = await Room.findByIdAndDelete(req.params.roomId);
   res.status(200).json({ success: true, result: { _id } });
--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
-import { createRoom, deleteRoom, getRooms } from "../controllers/room.js";
+import {
+  createRoom,
+  deleteRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
 import auth from "../middleware/auth.js";
 
 const roomRoute = Router();
 
-roomRoute.post("/create", auth, createRoom);
 roomRoute.get("/", getRooms);
+roomRoute.post("/create", auth, createRoom);
+roomRoute.patch("/:roomId", updateRoom);
 roomRoute.delete("/:roomId", deleteRoom);
 export default roomRoute;
