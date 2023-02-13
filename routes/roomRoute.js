068a1fd82b7b9d0b521460833ec49701f916ae64
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-02-13 17:25:36 +0530

    auth middleware added

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -6,12 +6,14 @@ import {
   getRooms,
   updateRoom,
 } from "../controllers/room.js";
+import roomAccess from "../middleware/accessControl/room/roomAccess.js";
 import auth from "../middleware/auth.js";
+import checkAccess from "../middleware/checkAccess.js";
 
 const roomRoute = Router();
 
 roomRoute.get("/", getRooms);
 roomRoute.post("/create", auth, createRoom);
-roomRoute.patch("/:roomId", updateRoom);
-roomRoute.delete("/:roomId", deleteRoom);
+roomRoute.patch("/:roomId", auth, checkAccess(roomAccess.update), updateRoom);
+roomRoute.delete("/:roomId", auth, checkAccess(roomAccess.delete), deleteRoom);
 export default roomRoute;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,15 +7,22 @@ import {
   updateProfile,
   updateStatus,
 } from "../controllers/user.js";
+import userAccess from "../middleware/accessControl/user/userAccess.js";
 import auth from "../middleware/auth.js";
+import checkAccess from "../middleware/checkAccess.js";
 
 const userRoute = Router();
 
 userRoute.post("/register", register);
 userRoute.post("/login", login);
 userRoute.patch("/updateProfile", auth, updateProfile);
-userRoute.get("/", getUsers);
-userRoute.patch("/updateStatus/:userId", updateStatus);
+userRoute.get("/", auth, checkAccess(userAccess.listUsers), getUsers);
+userRoute.patch(
+  "/updateStatus/:userId",
+  auth,
+  checkAccess(userAccess.updateStatus),
+  updateStatus
+);
 userRoute.delete("/:userId", deleteUser);
 
 export default userRoute;
